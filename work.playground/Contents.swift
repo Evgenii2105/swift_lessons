5348a92d2bfa0f24456ef547d18f86afc2bb84f4
Evgenii2105 <Fomev1@yandex.ru>
2024-07-07 18:41:04 +0300

    hw -1

--- work.playground/Contents.swift
+++ work.playground/Contents.swift
@@ -1,11 +1,25 @@
 import UIKit
 
-var a = 5
-
-print(a)
-
 var name = "Evgenii"
 
 var surname = "Fomichev"
 
-var b = 6
+var age: Int = 25
+
+var a = 134_564.35 + 59_001.61
+
+var b = Int(a) + age
+
+print(b)
+
+print("Evgenii Fomichev  \(age)")
+
+print(name + " " +  surname + " " + String(age))
+
+if age % 2 == 0 {
+    print("Evgenii \(a)")
+} else if age % 2 != 0 {
+    print(surname)
+}
+    
+
